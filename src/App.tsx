30e77d420f017dd7596c69eaaeb9c7643fa4c962
EduSerodio <dudu9090@outlook.com.br>
2023-10-24 14:29:20 -0300

    implementando renderização condicional

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import Card from "./components/card/Card"
 import Contador from "./components/contador/Contador"
 import Tarefa from "./components/tarefa/Tarefa"
 
+import Home from "./pages/home/Home"
+
 function App() {
     return (
         <>
             <Card titulo={"C shrap"} descricao={"Linguagem de programação"} />
             <Contador/>
             <Tarefa/>
+            <Home/>
         </>
     )
 }
